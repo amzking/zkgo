2ec15f8f273798e582f884a0066702cdb7ef32f5
amzking <amzking@foxmail.com>
2018-06-06 23:28:57 +0800

    add defaultMatcher

--- src/search/default.go
+++ src/search/default.go
@@ -9,6 +9,7 @@ func init() {
 
 /**
  * deaultMatcher 这个结构体实现量Matcher 的接口
+ * 若一个函数在声明的时候带有一个接收者，则意味着声明量一个方法，这个方法会和指定的接受者类型绑在一起。
  */
 func (m defaultMatcher) Search(feed *Feed, searchTerm string)([]*Result, error) {
 	return nil, nil
